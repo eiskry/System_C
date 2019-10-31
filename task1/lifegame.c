cd91c9bf6a973eb98cb12bdf02ff8f6fa3311fd7
ubuntu <ryotaeisaki0530@icloud.com>
2019-10-31 19:47:11 +0900

    lifegame

---
+++ task1/lifegame.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int v,s,n;
+    int i,j,k,l;
+    
+    char board1[100][100], board2[100][100];
+
+    //read board size
+    scanf("%d", &v);
+    scanf("%d",&s);
+
+
+    //first set board
+    for (i=0;i<v;i++){
+        for (j=0;j<2*s;j++){
+            board1[i][j]= ('O');
+        }
+    }
+
+    //print board
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < 2*s; j++) {
+            printf("%c", board1[i][j]);
+        }     printf("\n");
+    }
+   
+    
+
+    //set board
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < 2*s; j++) {
+         scanf("%c", &board1[i][j]);
+        }
+    }
+
+    //print board
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < 2*s; j++) {
+            printf("%c", board1[i][j]);
+        }     printf("\n");
+    }
+
+    //delete space
+    i=0;
+    j=0;
+    k=0;
+    while (i < v) {
+        for (j=0;j < 2*s;j++) {
+            if (board1[i][j]!=(' ')) {
+                board2[i][k]=board1[i][j];
+                k++;
+            }
+        }
+        i++;
+    }
+
+    printf("%d",i);
+    printf("%d",k);
+    
+
+    //print board
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < k; j++) {
+            printf("%c", board2[i][j]);
+        }     printf("\n");
+    }
+
+    //scan N
+    scanf("%d",&n);
+
+
+
+    printf("\n");
+
+
+    return 0;
+}

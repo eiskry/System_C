e5b1b9f57922d07b80cbd0293677b01d6d43800e
Ryota Eisaki <39875637+RyotaEisaki@users.noreply.github.com>
2019-11-01 18:23:26 +0900

    Update ex1_3.c

--- task1/ex1_3.c
+++ task1/ex1_3.c
@@ -6,18 +6,16 @@ int main(void)
   char str1[100], str2[100];
   int i, j;
 
+  //　標準入力から読み込んだ一行をstr1に格納
   scanf("%s", str1);
+
+  //　標準入力から読み込んだ次の行をstr2に格納
   scanf("%s", str2);
 
-  
-  i=0;
-  j=0;
-  
+  //　str1とstr2の文字を比較、同じなら出力
   for (i = 0; i < strlen(str1); i++)
     if (strchr(str2, str1[i]) != NULL)
       putc(str1[i], stdout);
-
-
-
+    
   return 0;
 }

b5d23e15ea16878829ffa985ecfa7ef515f93e93
ubuntu <ryotaeisaki0530@icloud.com>
2019-11-01 18:50:49 +0900

    edit

--- task1/ex1_2.c
+++ task1/ex1_2.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-  char str1[100],str2[100];
-  int i;
-  int n, tmp;
-  int digit;
+    int i;
+    char str[100];
 
-  scanf("%s", str1);
+    // input only digits
+    printf("input number: ");
+    scanf("%[0123456789]", str);
 
-  //　入力str1をint型に変換
-  n = atoi(str1);
-  tmp = n;
-  digit = 0;
+    // reverse string
+    printf("output number: ");
+    for (i = 1; i <= strlen(str); i++)
+        putc(str[strlen(str) - i], stdout);
+    puts("");
 
-  //  入力の整数の桁数を求める
-  do {
-    tmp /= 10;
-    digit++;
-  } while (tmp > 0);
-
-  //　str2にstr1の最後の要素から順に格納
-  for(i=0;i<digit;i++){
-    str2[i]=str1[digit-1-i];
-  }
-  str2[digit+1] = '\0';
-
-  //　str2の表示
-  printf("%s\n", str2);
-
-  return 0;
-}
+    return 0;
+} 

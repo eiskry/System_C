ce7b7e35c2e45b71bbb6cf90d62b21ce4782574f
Ryota Eisaki <39875637+RyotaEisaki@users.noreply.github.com>
2019-11-01 18:20:26 +0900

    Update ex1_2.c

--- task1/ex1_2.c
+++ task1/ex1_2.c
@@ -10,24 +10,24 @@ int main(void)
 
   scanf("%s", str1);
 
-  //change to int
+  //　入力str1をint型に変換
   n = atoi(str1);
   tmp = n;
   digit = 0;
 
+  //  入力の整数の桁数を求める
   do {
     tmp /= 10;
     digit++;
   } while (tmp > 0);
- 
-  // printf("%d\n", digit);
 
- 
+  //　str2にstr1の最後の要素から順に格納
   for(i=0;i<digit;i++){
     str2[i]=str1[digit-1-i];
   }
   str2[digit+1] = '\0';
 
+  //　str2の表示
   printf("%s\n", str2);
 
   return 0;

886629246f0ff0b624fcdba5b37094bbe2a462ca
ubuntu <ryotaeisaki0530@icloud.com>
2019-11-01 18:05:17 +0900

    lifegame Done

--- task1/lifegame.c
+++ task1/lifegame.c
@@ -5,49 +5,52 @@ int main(void) {
     int v,s,n;
     int i,j,k,l,m;
     int sum;
+    int gene;
     
-    char board1[100][100], board2[100][100],tmp[100][100];
+    char board1[100][100], board2[100][100];
+    int count[100][100], tmp[100][100],future[100][100];
 
     //read board size
     scanf("%d", &v);
     scanf("%d",&s);
 
 
-    //first set board
+    //first set board1 for  input format
     for (i=0;i<v;i++){
         for (j=0;j<2*s;j++){
             board1[i][j]= ('O');
         }
     }
 
-    //print board
-    for (i = 0; i < v; i++) {
-        for (j = 0; j < 2*s; j++) {
-            printf("%c", board1[i][j]);
-        }     printf("\n");
-    }
-   
+    // //print board1
+    // for (i = 0; i < v; i++) {
+    //     for (j = 0; j < 2*s; j++) {
+    //         printf("%c", board1[i][j]);
+    //     }     printf("\n");
+    // }
     
-
-    //set board
+    //set board1 for input
     for (i = 0; i < v; i++) {
         for (j = 0; j < 2*s; j++) {
          scanf("%c", &board1[i][j]);
         }
     }
 
-    //print board
-    for (i = 0; i < v; i++) {
-        for (j = 0; j < 2*s; j++) {
-            printf("%c", board1[i][j]);
-        }     printf("\n");
-    }
+    // //print board1
+    // printf("input");
+    // printf("\n");
+    // for (i = 0; i < v; i++) {
+    //     for (j = 0; j < 2*s; j++) {
+    //         printf("%c", board1[i][j]);
+    //     }     printf("\n");
+    // }
 
-    //delete space
+    //delete space to board2
     i=0;
     j=0;
     k=0;
     while (i < v) {
+        k=0;
         for (j=0;j < 2*s;j++) {
             if (board1[i][j]!=(' ')) {
                 board2[i][k]=board1[i][j];
@@ -55,136 +58,170 @@ int main(void) {
             }
         }
         i++;
+        
     }
 
+ 
+    // printf("\n");
+    // printf("--------------------------");
+    // printf("\n");
 
-
-    //print board
-    for (i = 0; i < v; i++) {
-        for (j = 0; j < k; j++) {
-            printf("%c", board2[i][j]);
-        }     printf("\n");
-    }
+    // //print board2
+    // printf("0th generation");
+    // printf("\n");
+    // for (i = 0; i < v; i++) {
+    //     for (j = 0; j < k; j++) {
+    //         printf("%c", board2[i][j]);
+    //     }     printf("\n");
+    // }
+    // printf("\n");
+    // printf("--------------------------");
 
     //scan N
     scanf("%d",&n);
 
-    //count alive cells
+    //////////////////////////////////////////////////////////
+
+    //o -> 1, . -> 0
     for (i = 0; i < v; i++) {
         for (j = 0; j < k; j++) {
-            sum=0;
-            if((i != 0)&&(j != 0)&&(i != v)&&(j != s)) {
-                for(l=-1;l<2;l++){
-                    for(m=-1;m<2;j++){
-                        if((l!=0) || (m!=0)) {
-                            sum ++;
-                        }
-                    }
-                }
-                tmp[i][j]=sum;
+            if(board2[i][j]==('O')) {
+                count[i][j]=1;
+            }
+            else if (board2[i][j]==('.')){
+                count[i][j]=0;
+            }
+            else if(board2[i][j]==(' ')) {
+                count[i][j]=(' ');
             }
         }
+    } 
+
+
+    //set tmp
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < s; j++) {
+                tmp[i][j]=count[i][j+1];
+        }    
     }
 
+//  printf("\n");
 
-    // sum=0;
-    // for(i = 0; i < v; i++) {
-    //     for(j = 0; j < s; j++) {
-    //         tmp[i][j] = board2[i][j];
-    //         //端っこのセルを除いての処理をする
-    //         if((i != 0)&&(j != 0)&&(i != v)&&(j != s)) {
-    //             for(k = -1; k < 2; k++) {
-    //                 for(l = -1; l < 2; l++) {
-    //                     if((k!=0) || (l!=0)) {
-    //                         sum += board2[i+k][j+l];
-    //                     } 
-    //                 }
-    //             } 
-    //             if(board2[i][j]) {
-    //             //セルの周りに2つか3つのセルがいたら生存
-    //                 if((sum != 2) && (sum != 3)) {
-    //                     tmp[i][j] = 0;
-    //                 }
-    //             }
-    //             //セルの周りに3つのセルがいたら誕生
-    //             else {
-    //                 if(sum == 3) {
-    //                     tmp[i][j] = 1;
-    //                 }
-    //             }
-    //         }
-    //         //それ以外は死滅
-    //         else {
-    //             tmp[i][j] = 0;
-    //         }
-    //     sum = 0;
-    //     }
-    // }
-    // for(i = 0;i < 20;i++){
-    //     for(j = 0;j < 20;j++){
-    //     board2[i][j] = tmp[i][j];
-    //     }
+//      for (i = 0; i < v; i++) {
+//         for (j = 0; j < s; j++) {
+//             printf("%d", tmp[i][j]);
+//         }     printf("\n");
+//     }
+
+    // for (i = 0; i < v; i++) {
+    //     for (j = 0; j < s; j++) {
+    //         future[i][j]=tmp[i][j];
+    //     }    
     // }
 
+    //////////////////////////////////////////////////////////
+
+
+    // for(gene=0;gene<n;gene++) {
     
 
-     //print board
+    //print 1th generaion
+    printf("\n");
+    printf("1th generation\n");
+    printf("\n");
     for (i = 0; i < v; i++) {
-        for (j = 0; j < k; j++) {
-            printf("%c", tmp[i][j]);
+        for (j = 0; j < s; j++) {
+          if(tmp[i][j]==1){
+              printf("%c",('O'));
+          } else {
+              printf("%c",('.'));
+          }
         }     printf("\n");
+    }
+    printf("\n");
+    printf("--------------------------");
+    printf("\n");
+
+     for (i = 0; i < v; i++) {
+            for (j = 0; j < s; j++) {
+                    future[i][j]=tmp[i][j];
+            }    
+        }
+
+    for (gene=0;gene<n-1;gene++){
+
+        //future -> tmp
+        for (i = 0; i < v; i++) {
+            for (j = 0; j < s; j++) {
+                    tmp[i][j]=future[i][j];
+            }    
+        }
+
+        for (int l = 0; l < v ; l++) 
+            { 
+                for (int m = 0; m < s ; m++) 
+                { 
+                    // finding no Of Neighbours that are alive 
+                    int aliveNeighbours = 0; 
+                    for (int i = -1; i <= 1; i++) 
+                        for (int j = -1; j <= 1; j++) 
+                            aliveNeighbours += tmp[l + i][m + j]; 
+    
+                    // The cell needs to be subtracted from 
+                    // its neighbours as it was counted before 
+                    aliveNeighbours -= tmp[l][m]; 
+    
+                    // Implementing the Rules of Life 
+    
+                    // Cell is lonely and dies 
+                    if ((tmp[l][m] == 1) && (aliveNeighbours < 2)) 
+                        future[l][m] = 0; 
+    
+                    // Cell dies due to over population 
+                    else if ((tmp[l][m] == 1) && (aliveNeighbours > 3)) 
+                        future[l][m] = 0; 
+    
+                    // A new cell is born 
+                    else if ((tmp[l][m] == 0) && (aliveNeighbours == 3)) 
+                        future[l][m] = 1; 
+    
+                    // Remains the same 
+                    else
+                        future[l][m] = tmp[l][m]; 
+                } 
+            } 
+
+     printf("\n");
+
+    //  for (i = 0; i < v; i++) {
+    //     for (j = 0; j < s; j++) {
+    //         printf("%d", future[i][j]);
+    //     }     printf("\n");
+    // }
+
+    //print result
+    printf("\n");
+    printf("%dth generation\n",gene+2);
+    printf("\n");
+    for (i = 0; i < v; i++) {
+        for (j = 0; j < s; j++) {
+          if(future[i][j]==1){
+              printf("%c",('O'));
+          } else {
+              printf("%c",('.'));
+          }
+        }     printf("\n");
+    }
+    printf("\n");
+    printf("--------------------------");
+    printf("\n");
+
     }
 
 
 
-
-    printf("\n");
+    
 
 
     return 0;
 }
-
-// void update(){
-//     int i, j, k, l;
-//     int sum = 0;
-//     int temp[v][s];
-    
-//     for(i = 0; i < v; i++) {
-//         for(j = 0; j < s; j++) {
-//             temp[i][j] = board2[i][j];
-// //端っこのセルを除いての処理をする
-//             if((i != 0)&&(j != 0)&&(i != 19)&&(j != 19)) {
-//                 for(k = -1; k < 2; k++) {
-//                     for(l = -1; l < 2; l++) {
-//                         if((k!=0) || (l!=0)) {
-//                             sum += board2[i+k][j+l];
-//                         } 
-//                     }
-//                 } 
-//                 if(board2[i][j]) {
-//                 //セルの周りに2つか3つのセルがいたら生存
-//                     if((sum != 2) && (sum != 3)) {
-//                         temp[i][j] = 0;
-//                     }
-//                 }
-//                 //セルの周りに3つのセルがいたら誕生
-//                 else {
-//                     if(sum == 3) {
-//                         temp[i][j] = 1;
-//                     }
-//                 }
-//             }
-//             //それ以外は死滅
-//             else {
-//                 temp[i][j] = 0;
-//             }
-//         sum = 0;
-//         }
-//     }
-//     for(i = 0;i < 20;i++){
-//         for(j = 0;j < 20;j++){
-//         board2[i][j] = temp[i][j];
-//         }
-//     }
-// }    
-

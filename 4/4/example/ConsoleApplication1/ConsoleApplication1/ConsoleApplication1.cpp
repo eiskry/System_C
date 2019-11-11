4a6ed0dbde1cada8f03b0086a57d153568d897e7
RyotaEisaki <39875637+RyotaEisaki@users.noreply.github.com>
2019-11-11 20:11:49 +0900

    Add files via upload

---
+++ 4/4/example/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,90 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <GL/glut.h>
+
+using namespace std;
+
+// 3次元ベクトルを扱うためのクラス
+class Vector3d {
+public:
+	double x, y, z;
+	Vector3d() { x = y = z = 0; }
+	Vector3d(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+	void set(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+
+	// 長さを1に正規化する
+	void normalize() {
+		double len = length();
+		x /= len; y /= len; z /= len;
+	}
+
+	// 長さを返す
+	double length() { return sqrt(x * x + y * y + z * z); }
+
+	// s倍する
+	void scale(const double s) { x *= s; y *= s; z *= s; }
+
+	// 加算の定義
+	Vector3d operator+(Vector3d v) { return Vector3d(x + v.x, y + v.y, z + v.z); }
+
+	// 減算の定義
+	// ★加算の定義を参考に、減算のコードを記述してみる
+
+	// 内積の定義
+	double operator*(Vector3d v) { return x * v.x + y * v.y + z * v.z; }
+
+	// 外積の定義
+	Vector3d operator%(Vector3d v) { return Vector3d(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+	// 代入演算の定義
+	Vector3d& operator=(const Vector3d& v) { x = v.x; y = v.y; z = v.z; return (*this); }
+
+	// 加算代入の定義
+	Vector3d& operator+=(const Vector3d& v) { x += v.x; y += v.y; z += v.z; return (*this); }
+
+	// 減算代入の定義
+	// ★加算代入の定義を参考に、減算代入のコードを記述してみる
+
+	// 値を出力する
+	void print() { printf("Vector3d(%f %f %f)\n", x, y, z); }
+};
+
+// マイナスの符号の付いたベクトルを扱えるようにするための定義 例：b=(-a); のように記述できる
+Vector3d operator-(const Vector3d& v) { return(Vector3d(-v.x, -v.y, -v.z)); }
+
+// ベクトルと実数の積を扱えるようにするための定義 例： c=5*a+2*b; c=b*3; のように記述できる
+Vector3d operator*(const double& k, const Vector3d& v) { return(Vector3d(k * v.x, k * v.y, k * v.z)); }
+Vector3d operator*(const Vector3d& v, const double& k) { return(Vector3d(v.x * k, v.y * k, v.z * k)); }
+
+// ベクトルを実数で割る操作を扱えるようにするための定義 例： c=a/2.3; のように記述できる
+Vector3d operator/(const Vector3d& v, const double& k) { return(Vector3d(v.x / k, v.y / k, v.z / k)); }
+
+
+int main(int argc, char** argv) {
+	// ベクトルの加算の例
+	Vector3d a(1, 1, 1);
+	Vector3d b(3, 2, -1);
+	Vector3d c = a + b;
+	c.print();
+
+	// Visual Studio でコンソールがすぐに閉じないようにするためには次のコメントアウトをはずす
+	//system("pause"); 
+	return 0;
+}
+
+
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します

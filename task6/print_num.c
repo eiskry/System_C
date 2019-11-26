668a02a19e7af46b5d9ce8adeae7fabbf586e63d
vaio_ubuntu <ryotaeisaki0530@icloud.com>
2019-11-26 18:05:49 +0900

    edit

---
+++ task6/print_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+    int n;
+    
+    if (argc != 2) {
+            printf("Usage: %s <number 1> <number 2>¥n", argv[0]);
+            return 0;
+    }
+    n = atoi(argv[1]);
+    
+    for(int i=0;i<n;i++){
+        printf("1\n");
+    }
+
+    return 0;
+}

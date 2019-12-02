cf151c820839c05b06f3350709395e77dc84c179
RyotaEisaki <39875637+RyotaEisaki@users.noreply.github.com>
2019-12-02 14:55:08 +0900

    Update ex5_5.c

--- task5/ex5_5.c
+++ task5/ex5_5.c
@@ -8,13 +8,15 @@ typedef struct trie {
   char c;
 } trie_t;
 
-void init_trie(trie_t *ptr)
-{
-  int i;
-  for (i = 0; i < 256; ++i) {
-    ptr->array[i] = NULL;
-  }
-  return;
+
+void init_trie(trie_t *ptr); //
+void add_to_trie(char *str, trie_t *ptr); //
+trie_t *find_in_trie(char *str, trie_t *ptr);
+
+void init_trie(trie_t *ptr) {
+  ptr->c = '\0';
+  ptr->next = NULL;
+  ptr->child = NULL;
 }
 
 trie_t *new_trie(void)
@@ -49,44 +51,45 @@ void add_to_trie(char *str, trie_t *ptr)
   }
 
   if (ptr->c == '\0') {
-    /* Implement by yourself */
+    ptr->c = *str;
+    ptr->child = new_trie();
+    add_to_trie(++str, ptr->child);
   } else if (ptr->c == *str) {
-    /* Implement by yourself */
+    add_to_trie(++str, ptr->child);
   } else if (ptr->next == NULL) {
-    /* Implement by yourself */
+    ptr->next = new_trie();
+    ptr->next->child = new_trie();
+    ptr->next->c = *str;
+    add_to_trie(++str, ptr->next->child);
   } else {
-    /* Implement by yourself */
+    add_to_trie(str, ptr->next);
   }
 }
 
-  /* Implement by yourself */
-trie_t *find_in_trie(char *str, trie_t *ptr)
-{
-  if (ptr == NULL)
-        return NULL;
-
-    if (strlen(str) == 0)
-        return ptr->array['.'];
-
-    return find_in_trie(str + 1, ptr->array[*str]);
-}
-
-void free_trie(trie_t *ptr)
-{
-  int i;
-  for (i = 0; i < 256; ++i) {
-    if (ptr->array[i] != NULL) {
-      free_trie(ptr->array[i]);
+trie_t *find_in_trie(char *str, trie_t *ptr) {
+  
+  if(strlen(str) == 0) {
+    if (ptr->c == '.') {
+      return ptr;
+    } else if (ptr->next == NULL) {
+      return NULL;
+    } else {
+      find_in_trie(str, ptr->next);
     }
+  } else if (ptr->c == *str) {
+    find_in_trie(++str, ptr->child);
+  } else if (ptr->next == NULL) {
+    return NULL;
+  } else {
+    find_in_trie(str, ptr->next);
   }
-  free(ptr);
 }
 
 int main(int argc, char *argv[])
 {
   FILE *f;
   char str[512];
-  trie_t *root;
+  trie_t root;
 
   if (argc != 3) {
     fprintf(stderr, "Missing argument\n");
@@ -98,23 +101,20 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  root = new_trie();
+  init_trie(&root);
   while (fgets(str, 512, f)) {
     if (str[strlen(str) - 1] == '\n') {
       str[strlen(str) - 1] = '\0';
     }
-    add_to_trie(str, root);
+    add_to_trie(str, &root);
   }
 
   fclose(f);
 
-  if (find_in_trie(argv[2], root) != NULL) {
+  if (find_in_trie(argv[2], &root) != NULL) {
     printf("%s: \"%s\" is found in %s\n", argv[0], argv[2], argv[1]);
   } else {
     printf("%s: \"%s\" is not found in %s\n", argv[0], argv[2], argv[1]);
   }
-
-  free_trie(root);
-
   return 0;
 }

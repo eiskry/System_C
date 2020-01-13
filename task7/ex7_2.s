fb1afd4e0137bdf0fa49dbdee39c12b0b9d4f2cd
vaio_ubuntu <ryotaeisaki0530@icloud.com>
2020-01-13 17:05:31 +0900

    task7

--- task7/ex7_2.s
+++ task7/ex7_2.s
@@ -1,8 +1,11 @@
         .text
         .globl main
-main:   mov $1,%eax
-        mov $2,%ebx
-        mov %eax,%ecx
-        add %ebx,%ecx
-        call finish
+main:   mov $0 ,%eax #loop count
+        mov $0 ,%edx #add result
+    L1:  cmp $10, %eax
+        je L2
+        add $1,%eax
+        add %eax, %edx
+        jmp L1
+    L2: call finish
         

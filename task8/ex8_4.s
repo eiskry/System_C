58cba114b961806a4538782f0a590d78ffd7ad6b
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-02-03 21:00:54 +0900

    8_4

--- task8/ex8_4.s
+++ task8/ex8_4.s
@@ -5,20 +5,21 @@ z: .long 0, 0, 0, 0
 .text
 .global main
 main:
-  mov $0, %eax #q
-  mov $0, %edx #r
-
-  mov $0, %ebx
-  mov $0, %ecx
-
-  mov $0, %esi #step
-
-  mov $0, %edi #hinan
-
-
-  add x(,%esi,4), %eax
-  add y(,%esi,4), %ebx
-
-  idiv ebx
-
-  # mov src1, %eax
+        mov $0, %esi
+.L1:    cmp $4, %esi
+        je .L2
+        mov x(,%esi,4), %eax
+        cltd
+        idivl y(,%esi,4)
+        mov %eax, z(,%esi,4)
+        inc %esi
+        jmp .L1
+.L2:    mov $0, %esi
+        mov z(,%esi,4), %eax
+        inc %esi
+        mov z(,%esi,4), %ebx
+        inc %esi
+        mov z(,%esi,4), %ecx
+        inc %esi
+        mov z(,%esi,4), %edx
+        call finish

2d15564caf4376ae5f2c5d7495fa85275bed62ba
vaio_ubuntu <ryotaeisaki0530@icloud.com>
2020-02-14 15:21:24 +0900

    edit

--- task9/ex9_3.s
+++ task9/ex9_3.s
@@ -1,18 +1,18 @@
 .text
 .global sum
 sum:
-  push %ebp
-  mov %esp, %ebp
-  mov $0, %eax
-  mov 8(%ebp), %edx
-  dec %edx
-  mov 12(%ebp), %ecx
-  jmp L
+    push %ebp
+    mov %esp, %ebp
+    mov $0, %eax
+    mov 8(%ebp), %edx
+    dec %edx
+    mov 12(%ebp), %ecx
+    jmp L
 L:
-  add (%ecx,%edx,4), %eax
-  dec %edx
-	js L2
-  jmp L
+    add (%ecx,%edx,4), %eax
+    dec %edx
+        js L2
+    jmp L
 L2:
-  leave
-  ret
+    leave
+    ret

911566031d2cad380f8e0331f9a9dc753f8ac004
vaio_ubuntu <ryotaeisaki0530@icloud.com>
2020-02-14 14:37:46 +0900

    task9

---
+++ task9/ex9_1.s
@@ -0,0 +1,53 @@
+  .text
+  .global main
+L0:   //  x == 0 の処理
+  mov $0, %eax
+  leave
+  ret
+
+L1:   //  x == 1 の処理
+  mov $1, %eax
+  leave
+  ret
+
+fib:
+  // 旧 ebp 待避
+  push %ebp
+  // 新 ebp セット
+  mov %esp, %ebp
+
+  // x == 1
+  cmp $1, %eax
+  je L1
+  // x == 0
+  cmp $0, %eax
+  je L0
+
+  // x の待避
+  push %eax
+  //	fib (x - 1)
+  sub $1, %eax
+  call fib
+  // 計算結果を %ecx に
+  mov %eax, %ecx
+
+  // x の復帰
+  pop %eax
+
+  //	%ecx = fib (x - 1) を待避
+  push %ecx
+  //	fib (x - 2)
+  sub $2, %eax
+  call fib
+  // %ecx の復帰
+  pop %ecx
+
+  add %ecx, %eax
+
+  leave
+  ret
+
+main:
+  mov $20, %eax
+  call fib
+  call finish
---
+++ task9/ex9_2.s
@@ -0,0 +1,16 @@
+.text
+.global determinant
+determinant:
+  push %ebp
+  mov %esp, %ebp
+
+  mov 8(%ebp), %eax
+  mov 12(%ebp), %ecx
+  imul 16(%ebp), %ecx
+  imul 20(%ebp), %eax
+
+  sub %ecx, %eax
+
+  leave
+  ret
+  

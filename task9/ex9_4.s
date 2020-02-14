c4395bb40266a7cddf465dd8a697728e01fa9c85
vaio_ubuntu <ryotaeisaki0530@icloud.com>
2020-02-14 14:59:33 +0900

    edit

---
+++ task9/ex9_4.s
@@ -0,0 +1,31 @@
+  .text
+  .global make_allone_array
+make_allone_array:
+  push %ebp
+  mov %esp, %ebp
+
+  mov 8(%ebp), %ecx
+  sub $1, %ecx
+
+	push %edx
+  push %ecx
+
+  mov 8(%ebp), %eax
+  imul $4, %eax
+  push %eax
+  call malloc
+
+  add $4, %esp
+  /*pop %ecx*/
+  pop %ecx
+  pop %edx
+  jmp L
+L:
+  add $1, (%eax,%ecx,4)
+  sub $1, %ecx
+  js L2
+  jmp L
+
+L2:
+  leave
+  ret
